82088f2505ad6b6382b232626a9b52ecc73feb65
william.chenta <william.chenta@gmail.com>
2015-05-20 15:24:59 -0300

    Commit numero: 2 Primeiro commit. mapeando entidade aluno para JPA.

--- src/br/edu/senai/model/Aluno.java
+++ src/br/edu/senai/model/Aluno.java
@@ -1,14 +1,25 @@
 package br.edu.senai.model;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 @Entity(name = "tb_aluno")
 public class Aluno implements Serializable {
 
+    @Id
+    @Column(name = "id_aluno")
+    private int id;
+    
     @OneToOne
+    @JoinColumn(name = "id", unique = true)
     private Pessoa pessoa;
+
+    public Aluno() {
+    }
 
     public Aluno(Pessoa pessoa) {
         this.pessoa = pessoa;
@@ -21,4 +32,8 @@ public class Aluno implements Serializable {
     public void setPessoa(Pessoa pessoa) {
         this.pessoa = pessoa;
     }    
+
+    public int getId() {
+        return id;
+    }
 }
