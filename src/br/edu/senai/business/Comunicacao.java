b168476b525a2746d17ae4736a2f59d326cee3aa
William Chenta <william.chenta@gmail.com>
2015-05-25 20:29:08 -0300

    só um teste

--- src/br/edu/senai/business/Comunicacao.java
+++ src/br/edu/senai/business/Comunicacao.java
@@ -2,7 +2,7 @@ package br.edu.senai.business;
 
 import br.edu.senai.model.Pessoa;
 import java.util.List;
-
+//teste
 public interface Comunicacao {
     public void comunicar(List<Pessoa> pessoas);
     public void comunicar(Pessoa pessoa);
